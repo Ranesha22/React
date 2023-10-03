637e2a495d6bbd2f06ff679808ff5ff33d727ae3
Ranesha Pal <raneshapal@gmail.com>
2023-10-04 00:03:38 +0530

    ep-01

---
+++ Episode-01/App.js
@@ -0,0 +1,21 @@
+
+/**
+ * <div id=parent>
+ *      <div id=child>
+ *          <h1></h1>
+ *          <h2></h2>
+ *      </div>
+ * </div>
+ * 
+ */
+
+
+// parent is an object
+const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I am an h1 tag"),React.createElement("h2",{},"I am h2 tag")]))
+const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+console.log(parent)
+
+// render converts object into html
+root.render(parent)
